f945a86efc786568736e3076dc456ca1dce3961c
Dominic Cartwright <dominic.cartwright07@gmail.com>
2020-07-22 14:28:33 -0400

    Moved javascript to external file

---
+++ nav.js
@@ -0,0 +1,48 @@
+function mainMenuHover(event) {
+
+	let el = $('#' + event.target.id);
+	let sibling = el.prev();
+
+	if(el.hasClass('is-open')) {
+		sibling.addClass('visible-link');
+	} else {
+		sibling.removeClass('visible-link');
+	}
+
+}
+
+function toggleNav(self) {
+
+	let el = $("#myNav");
+	let height = el.css("height");
+	self.classList.toggle("change");
+
+	if(height == "0px") {
+		el.css("height", "100%");
+	} else {
+		el.css("height", "0%");
+	}
+
+}
+
+function showCtaOptions() {
+
+	let specificHeight = 250
+	let delayInMilliseconds = 300;
+	let scroll = $(window).scrollTop();
+
+	if(scroll >= specificHeight) {
+		setTimeout(function () {
+
+			$('.addmissions-side-menu').removeClass('top');
+
+		}, delayInMilliseconds);
+	} else {
+		setTimeout(function () {
+
+			$('.addmissions-side-menu').addClass('top');
+
+		}, delayInMilliseconds);
+	}
+
+}
